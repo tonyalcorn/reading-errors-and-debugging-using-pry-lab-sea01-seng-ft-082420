209438d164709ecd8282ab766bf00d408207d33b
Anthony Alcorn <xmastony@gmail.com>
2020-08-11 18:37:40 +0000

    Automatically backed up by Learn

--- spec/method_man_spec.rb
+++ spec/method_man_spec.rb
@@ -8,6 +8,6 @@ end
 
 describe "method_man" do
   it "`play_game` should return a string that states which player is better" do
-    expect(play_game("Jay-Z", "Beyonce")).to eq("Jay-Z is better than Beyonce")
+    expect(play_game("Jay-Z", "Beyonce")) == ("Jay-Z is better than Beyonce")
   end
 end
