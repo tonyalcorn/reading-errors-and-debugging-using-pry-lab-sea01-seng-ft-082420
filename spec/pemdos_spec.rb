561d7a8e6962d71bb0d50301b88c70af01a3c4a0
Anthony Alcorn <xmastony@gmail.com>
2020-08-11 18:43:55 +0000

    Automatically backed up by Learn

--- spec/pemdos_spec.rb
+++ spec/pemdos_spec.rb
@@ -2,6 +2,6 @@ require_relative '../fix_using_tests/pemdos'
 
 describe "pemdos" do
   it "`snake_it_up` adds the letter 's' ten times before any word that starts with 's'" do
-  expect(snake_it_up("surprise!")).to eq("sssssssssssurprise!")
+  expect(snake_it_up("surprise!")) == ("sssssssssssurprise!")
   end
 end
