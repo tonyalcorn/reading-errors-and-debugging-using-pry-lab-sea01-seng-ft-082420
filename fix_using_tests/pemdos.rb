b6118298f9f6c08f64885dcded9285e8b9d972cd
Anthony Alcorn <xmastony@gmail.com>
2020-08-11 18:41:07 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,11 @@
-# don't forget to add: require 'pry'
+require pry# don't forget to add: require 'pry'
 
 def snake_it_up(string)
   if string[0] == "s"
     10 * "s" + string
+    binding.pry
   else
     string
+    binding.pry
   end
 end
