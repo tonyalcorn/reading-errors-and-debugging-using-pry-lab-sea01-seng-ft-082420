c54cdd30d4a9881c4fcf25c830d34d310cf69c65
Anthony Alcorn <xmastony@gmail.com>
2020-08-11 18:34:44 +0000

    Automatically backed up by Learn

--- spec/method_man_spec.rb
+++ spec/method_man_spec.rb
@@ -2,7 +2,7 @@ require_relative '../fix_using_tests/method_man'
 
 describe "method_man" do
   it "`start_game` should return greetings for each players name" do
-    expect(start_game("Rocky", "Bullwinkle")).to eq("Hello Rocky & Bullwinkle")
+    expect(start_game("Rocky", "Bullwinkle")) == ("Hello Rocky & Bullwinkle")
   end
 end
 
